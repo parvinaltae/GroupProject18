d398c9f2c680ff99340849cd51abda82c0c82c4b
Aziz Yuldashev <aziz929292@mail.ru>
2021-06-30 21:54:53 -0400

    TS_010_Aziz finished

--- src/test/java/Group18/TS_010_Aziz.java
+++ src/test/java/Group18/TS_010_Aziz.java
@@ -4,6 +4,7 @@ public class TS_010_Aziz {
     public static void main(String[] args) {
 
         System.out.println("TS_010 has started, get ready, we are going UP!!!");
+        System.out.println("Finished my TS");
 
 
     }

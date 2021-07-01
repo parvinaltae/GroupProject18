659ef2dcff89fb3eba13fbbe4950ce7caee9b93c
Parvin <parvin.altae@gmail.com>
2021-06-30 21:35:06 -0400

    initial commit

---
+++ src/test/java/Group18/Testing.java
@@ -0,0 +1,8 @@
+package Group18;
+
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Hello Cyberbugs");
+        System.out.println("Calling all Cyberbugs");
+    }
+}

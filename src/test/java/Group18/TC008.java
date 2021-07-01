200eff27b40d15e8009da8f72351701ff138e1e4
Oogii214 <oogii07_10@yahoo.com>
2021-06-30 22:11:16 -0400

    change added again

--- src/test/java/Group18/TC008.java
+++ src/test/java/Group18/TC008.java
@@ -3,6 +3,6 @@ package Group18;
 public class TC008 {
     public static void main(String[] args) {
 
-        System.out.println("Where is others");
+        System.out.println("Where is others ");
     }
 }

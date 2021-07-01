96da6408676c54f61cb7e2095724f64b86419744
Aziz Yuldashev <aziz929292@mail.ru>
2021-06-30 21:53:28 -0400

    TS_010_Aziz started

---
+++ src/test/java/Group18/TS_010_Aziz.java
@@ -0,0 +1,10 @@
+package Group18;
+
+public class TS_010_Aziz {
+    public static void main(String[] args) {
+
+        System.out.println("TS_010 has started, get ready, we are going UP!!!");
+
+
+    }
+}

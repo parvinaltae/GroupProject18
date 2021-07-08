e4ab5dbb255c1e0f911525980ae8cb11154464bd
Oogii214 <oogii07_10@yahoo.com>
2021-07-07 21:10:20 -0400

    hello class

---
+++ src/test/java/Group18/Hello.java
@@ -0,0 +1,9 @@
+package Group18;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Are you guys working on it?");
+
+        //Awesome cucumber stuff here
+    }
+}
